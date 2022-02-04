8df3657fa07eafa30dcc0406426c5e64c55f6e7c
Mini <80361877+Mini51@users.noreply.github.com>
2022-02-04 18:48:41 -0500

    added a a way to choose button amount and a default RPC

--- index.js
+++ index.js
@@ -1,26 +1,91 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+const DiscordRPC = require('discord-rpc');
+const { clientId } = require('./config.json')
+var prompt = require('prompt-sync')();
+var activity = require('./defaultRPC.json')
+const startTimestamp = new Date();
+
+//Ask the user how many buttons they want on there RPC
+console.log('Select the amount of buttons you want \n A - no buttons \n B - 1 button \n C - 2 buttons \n NOTE: you can press enter to load the RPC from defaulrRPC.json'); 
+var  RPCtype = prompt(' ').toLowerCase();
 
 
+// Check what option the user chose 
+switch(RPCtype){
 
-//CREATING ELECTRON WINDOW 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }  
-  })
+    case "a": 
+        console.log("you selected 0 button");
+        var details = prompt('Input The detail field(Must be more then 2 chars)');
+        var state = prompt('what do you want the state to be(Must be more then 2 chars)')
 
-  win.loadFile('index.html')
+         activity = {
+          details: details ,
+          state: state ,
+          largeImageKey: 'snek_large',
+          largeImageText: 'tea is delicious',
+          smallImageKey: 'snek_small',
+          smallImageText: 'i am my own pillows',
+          instance: false,
+        }
+        break; 
+    case "b":
+        console.log("you selected 1 button");
+        var details = prompt('Input The detail field(Must be more then 2 chars)');
+        var state = prompt('what do you want the state to be(Must be more then 2 chars)')
+        var buttonName = prompt('What do you want the button name to be(Must be more then 2 chars)') 
+        var buttonURL = prompt('What do you want the button to link to(Must be a valid URL)')
+
+
+         activity = {
+          buttons: [{ label: buttonName, url: buttonURL}],
+          details: details ,
+          state: state ,
+          largeImageKey: 'snek_large',
+          largeImageText: 'tea is delicious',
+          smallImageKey: 'snek_small',
+          smallImageText: 'i am my own pillows',
+          instance: false,
+        }
+        break;
+    case "c":
+        console.log("you selected 2 buttons");
+        break;
+    default: 
+        console.log('Loading RPC from defaultRPC.json')
+        break;
 }
 
-app.whenReady().then(() => {
-  createWindow()
-})
 
-ipcMain.handle('quit-app', () => {
-  app.quit();
-});
-//all the rpc shit is in the preload 
+
+
+
+
+
+// Register with the clientId 
+DiscordRPC.register(clientId);
+
+//Define the rpc client 
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+// Start the timer 
+
+
+
+
+async function setActivity() {
+    if (!rpc) {
+      return;
+    }
+  
+    rpc.setActivity(activity);
+  }
+  
+  rpc.on('ready', () => {
+    setActivity();
+    console.log(`\x1b[32mActivity has been set successfully.  \x1b[0m`)
+
+    setInterval(() => {
+      setActivity();
+    }, 15e3);
+  });
+  
+rpc.login({ clientId }).catch(console.error);
