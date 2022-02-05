506f44045c9bdb7ca61643c3e47499f841871407
Mini <80361877+Mini51@users.noreply.github.com>
2022-02-04 18:42:34 -0800

    Final commit before public

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var activity = require('./defaultRPC.json')
 const startTimestamp = new Date();
 
 //Ask the user how many buttons they want on there RPC
-console.log('Select the amount of buttons you want \n A - no buttons \n B - 1 button \n C - 2 buttons \n NOTE: you can press enter to load the RPC from defaulrRPC.json'); 
+console.log('Select the amount of buttons you want \n A - no buttons \n B - 1 button \n C - 2 buttons \n\x1b[33mNOTE: you can press enter to load the RPC from defaultRPC.json \x1b[0m'); 
 var  RPCtype = prompt(' ').toLowerCase();
 
 
@@ -15,10 +15,15 @@ switch(RPCtype){
     case "a": 
         console.log("you selected 0 button");
         var details = prompt('Input The detail field(Must be more then 2 chars)');
+        console.log(details)
         var state = prompt('what do you want the state to be(Must be more then 2 chars)')
         var largeImage = ('Input the large Image key')
         var largeImageText = ('Input large Image text')
 
+        if(details || state === ""){
+          console.log('\x1b[31m [ERROR]You did not give info for one of the required fields \x1b[0m');
+          process.exit();
+        } else { 
          activity = {
           details: details ,
           state: state ,
@@ -26,6 +31,7 @@ switch(RPCtype){
           largeImageText: largeImageText,
           instance: false,
         }
+       } 
         break; 
     case "b":
         console.log("you selected 1 button");
@@ -36,24 +42,21 @@ switch(RPCtype){
         var buttonName = prompt('What do you want the button name to be(Must be more then 2 chars)') 
         var buttonURL = prompt('What do you want the button to link to(Must be a valid URL)')
 
-        activity = {
-          details: details ,
-          state: state ,
-          largeImageKey: largeImage ,
-          largeImageText: largeImageText ,
-          instance: false,
-        }
-
-         activity = {
-          buttons: [{ label: buttonName, url: buttonURL}],
-          details: details ,
-          state: state ,
-          largeImageKey: 'snek_large',
-          largeImageText: 'tea is delicious',
-          smallImageKey: 'snek_small',
-          smallImageText: 'i am my own pillows',
-          instance: false,
-        }
+        if(details || state || buttonName || buttonURL === ""){
+          console.log('\x1b[31m [ERROR]You did not give info for one of the required fields \x1b[0m');
+          process.exit();
+        } else {
+          activity = {
+            buttons: [{ label: buttonName, url: buttonURL}],
+            details: details ,
+            state: state ,
+            largeImageKey: 'snek_large',
+            largeImageText: 'tea is delicious',
+            smallImageKey: 'snek_small',
+            smallImageText: 'i am my own pillows',
+            instance: false,
+          } 
+        } 
         break;
     case "c":
         console.log("you selected 2 buttons");
@@ -67,21 +70,26 @@ switch(RPCtype){
         var buttonTwoName = prompt('What do you want the second buttons name to be(Must be more then 2 chars)') 
         var buttonTwoURL = prompt('What do you want the second buttons to link to(Must be a valid URL)')
 
-         activity = {
-          buttons: [{ label: buttonName, url: buttonURL}, { label: buttonTwoName, url: buttonTwoURL}],
-          details: details ,
-          state: state ,
-          largeImageKey: 'snek_large',
-          largeImageText: 'tea is delicious',
-          smallImageKey: 'snek_small',
-          smallImageText: 'i am my own pillows',
-          instance: false,
-        }
+          if(state || buttonName || buttonURL || buttonTwoName || buttonTwoURL === ""){ 
+            console.log('\x1b[31m [ERROR]You did not give info for one of the required fields \x1b[0m');
+            process.exit();
+          } else { 
 
+          activity = {
+            buttons: [{ label: buttonName, url: buttonURL}, { label: buttonTwoName, url: buttonTwoURL}],
+            details: details ,
+            state: state ,
+            largeImageKey: 'snek_large',
+            largeImageText: 'tea is delicious',
+            smallImageKey: 'snek_small',
+            smallImageText: 'i am my own pillows',
+            instance: false,
+          }
+        } 
 
         break;
     default: 
-        console.log('Loading RPC from defaultRPC.json')
+        console.log('\x1b[33mLoading RPC from defaultRPC.json')
         break;
 }
 
