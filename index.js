9044c250fcfe4b222f03b6b7453acf91a40f58f1
Mini <80361877+Mini51@users.noreply.github.com>
2022-02-06 11:07:11 -0800

    put the setup in main file
    Makes it easier for the user to setup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+//importing modules
+const { Console } = require('console');
 const DiscordRPC = require('discord-rpc');
-const { clientId } = require('./config.json')
-var prompt = require('prompt-sync')();
+const fs = require('fs');
+const prompt = require('prompt-sync')();
 var activity = require('./defaultRPC.json')
-
-//Defining console colors
 const Color = {
   Reset: "\x1b[0m",
   
@@ -17,8 +17,9 @@ const Color = {
 }
 
 
-
-
+// Check if the config exsits if it does not it will start the setup
+if (fs.existsSync('./config.json')) {
+ 
 
 //Starting timer 
 const startTimestamp = Date.now()
@@ -26,6 +27,9 @@ const startTimestamp = Date.now()
 //Ask the user how many buttons they want on there RPC
 console.log('Select the amount of buttons you want \n A - no buttons \n B - 1 button \n C - 2 buttons \n\x1b[33mNOTE: you can press enter to load the RPC from defaultRPC.json \x1b[0m'); 
 var  RPCtype = prompt(' ').toLowerCase();
+
+//Get the client ID from the config
+const { clientId } = require('./config.json');
 
 
 // Check what option the user chose 
@@ -132,3 +136,24 @@ async function setActivity() {
   });
   
 rpc.login({ clientId }).catch(console.error);
+} else { 
+  console.log(Color.FgYellow,"Since this is the first time using this tool, we have a bit of setup to do.", Color.Reset)
+  var cliendID = prompt(' Please input your clientID:')
+
+
+    if(cliendID === ""){
+        console.log(Color.FgRed, '[ERROR] You did not input your clientID', Color.Reset)
+        return
+    }
+    // What needs to be written
+    let Data = { 
+        clientId: cliendID
+    }
+
+    //Converting so that the data can be sent
+    let formatedData = JSON.stringify(Data);
+
+    fs.writeFile('config.json', formatedData, () =>{
+      console.log(Color.FgGreen, 'Config file has been created, Please rerun this app to use the RPC maker.', Color.Reset)
+    })
+}
